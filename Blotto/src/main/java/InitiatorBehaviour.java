2227fbc076735921dc7a17cfcaeb070b890ced0e
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-01-14 12:25:47 +0100

    Started implementing InitiatorBehaviour: - moved DF lookup from PlayBehaviour to InitiatorBehaviour, - added sending out basic CFP.

--- Blotto/src/main/java/InitiatorBehaviour.java
+++ Blotto/src/main/java/InitiatorBehaviour.java
@@ -1,15 +1,57 @@
 import jade.core.Agent;
 import jade.core.behaviours.DataStore;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import jade.proto.ContractNetInitiator;
+import java.util.Vector;
+import java.util.logging.Logger;
 
 
 public class InitiatorBehaviour extends ContractNetInitiator {
+    DFAgentDescription[] agents = null;
 
     public InitiatorBehaviour(Agent a, ACLMessage cfp, DataStore store) {
         super(a, cfp, store);
     }
 
-    // Move current play behaviour here here.
+    @Override
+    public void onStart() {
+        Logger.getGlobal().info("InitiatorBehaviour: Getting agents from DF");
+
+        try {
+            // Obtaining known agents from DF.
+            DFAgentDescription dfd = new DFAgentDescription();
+            ServiceDescription sd  = new ServiceDescription();
+            sd.setType("Blotto");
+            dfd.addServices(sd);
+
+            agents = DFService.search(this.myAgent, dfd);
+
+            System.out.println(agents.length + " results" );
+
+        } catch (FIPAException ex) {
+            //FIXME
+        }
+        // TODO send CFPs
+        // Pair up
+    }
+
+    @Override
+    protected Vector prepareCfps(ACLMessage cfp) {
+        Vector<ACLMessage> msgs = new Vector<ACLMessage>();
+        System.out.println(cfp);
+        for (int i = 0; i < agents.length; ++i) {
+            ACLMessage newMsg = new ACLMessage(ACLMessage.CFP);
+            newMsg.setProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
+            newMsg.addReceiver(agents[i].getName());
+            newMsg.addReplyTo(myAgent.getAID());
+            msgs.add(newMsg);
+        }
+        return msgs;
+    }
 
 }
--- Blotto/src/main/java/PlayBehaviour.java
+++ Blotto/src/main/java/PlayBehaviour.java
@@ -11,30 +11,6 @@ public class PlayBehaviour extends Behaviour {
 
     @Override
     public void action() {
-        Logger.getGlobal().info("Action in playBehaviour");
-        // TODO
-        // Get agents from DF
-        try {
-            // Obtaining known writers from DF
-            DFAgentDescription dfd = new DFAgentDescription();
-            ServiceDescription sd  = new ServiceDescription();
-            sd.setType("Blotto");
-            dfd.addServices(sd);
-
-            DFAgentDescription[] result = null;
-            result = DFService.search(this.myAgent, dfd);
-
-            System.out.println(result.length + " results" );
-            if (result.length > 0) {
-                System.out.println(" " + result[0].getName());
-            }
-
-        } catch (FIPAException ex) {
-            //FIXME
-        }
-        // Start CNP
-        // Pair up
-
     }
 
     @Override
